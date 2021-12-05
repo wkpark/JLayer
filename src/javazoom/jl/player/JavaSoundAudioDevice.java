2bb6c56ba1c9607eb1fa42bd648b3544d4eec0ab
Perry <80945555+Perry-da-Platypus@users.noreply.github.com>
2021-12-05 17:26:58 +0100

    fixed javax.sound.sampled.FloatControl import

--- src/javazoom/jl/player/JavaSoundAudioDevice.java
+++ src/javazoom/jl/player/JavaSoundAudioDevice.java
@@ -27,6 +27,7 @@ package javazoom.jl.player;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
